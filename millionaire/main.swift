0b3e656de7f1b502635db9189d0bb748fed8d600
Louise Chan <lbchan525@gmail.com>
2020-03-18 15:46:22 -0400

    Initial Commit

---
+++ millionaire/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  millionaire
+//
+//  Created by Louise Chan on 2020-03-18.
+//  Copyright © 2020 Louise Chan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
